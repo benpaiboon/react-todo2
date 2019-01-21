889738b8fc0ba52f643f126418e6caf741a1899f
Paiboon SIRAWIT <paiboon.sirawit@asia.essilor.group>
2019-01-21 17:14:24 +0700

    add html mockup for AR Fast Setup

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,7 +1,12 @@
 import React, { Component } from 'react'
 import ReactToExcel from 'react-html-table-to-excel';
+import axios from 'axios';
 
 class Table extends Component {
+  componentDidMount() {
+    axios.get('http://localhost:5000/data')
+      .then(res => console.log(res))
+  }
   render() {
     return (
       <div>
@@ -11,30 +16,43 @@ class Table extends Component {
           sheet="1"
           buttonText="Export"
         />
-        
+
         <table id="table-export">
           <thead>
             <tr>
-              <th>Firstname</th>
-              <th>Lastname</th>
-              <th>Department</th>
+              <th></th>
+              <th colSpan="2">Concave Surface (CC)</th>
+              <th colSpan="2">Convexe Surface (CX)</th>
+              <th colSpan="2">Machine Recipe</th>
+            </tr>
+            <tr>
+              <th>#</th>
+              <th>Usability</th>
+              <th>Ratio	</th>
+              <th>Usability</th>
+              <th>Ratio	</th>
+              <th>CC</th>
+              <th>CX</th>
             </tr>
           </thead>
           <tbody>
             <tr>
-              <td>1</td>
-              <td>1</td>
-              <td>1</td>
+              <td>Q0</td>
+              <td>Bill Gates</td>
+              <td>555 77 854</td>
+              <td>555 77 855</td>
+              <td>555 77 855</td>
+              <td>1.7</td>
+              <td>16.1</td>
             </tr>
             <tr>
-              <td>2</td>
-              <td>2</td>
-              <td>2</td>
-            </tr>
-            <tr>
-              <td>3</td>
-              <td>3</td>
-              <td>3</td>
+              <td>Z0</td>
+              <td>Bill Gates</td>
+              <td>555 77 854</td>
+              <td>555 77 855</td>
+              <td>555 77 855</td>
+              <td>4</td>
+              <td>121</td>
             </tr>
           </tbody>
         </table>
