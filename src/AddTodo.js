f69192f278454516f1f5bfcc93083df5d542d8aa
unknown <paiboon@setscope.com>
2019-03-03 15:20:38 +0700

    todo reset state from parent example

--- src/AddTodo.js
+++ src/AddTodo.js
@@ -1,27 +1,26 @@
 import React, { Component } from 'react';
 
 class AddTodo extends Component {
-  state ={
-    content: ''
-  }
-  handleChange = (e) => {
-    this.setState({
-      content: e.target.value
-    })
-  }
-  handleSubmit = (e) => {
-    e.preventDefault();
-    this.props.addTodo(this.state);
-    this.refs.todoForm.reset();
-  }
+  // state ={
+  //   content: ''
+  // }
+  // handleChange = (e) => {
+  //   this.setState({
+  //     content: e.target.value
+  //   })
+  // }
+  // handleSubmit = (e) => {
+  //   e.preventDefault();
+  //   this.props.addTodo(this.state);
+  //   this.refs.todoForm.reset();
+  // }
+
   render() {
     return (
-      <div className="AddTodo">
-        <form ref="todoForm" onSubmit={this.handleSubmit}>
-          <label>Add Todo: {this.state.content}</label>
-          <input type="text" onChange={this.handleChange}></input>
-        </form>
-      </div>
+      <form onSubmit={this.props.handleSubmit}>
+        <label>Add Todo: {this.props.allState.content}</label>
+        <input type="text" onChange={this.props.handleChange} value={this.props.allState.content} ></input>
+      </form>
     );
   }
 }
--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import Home from './components/Home';
-import Photos from './components/Photos';
-import About from './components/About';
-import Contact from './components/Contact';
-import FcTable from './components/FcTable';
-import Table from './components/Table';
-import Post from './components/Post';;
-// import Todos from './Todos';
-// import AddTodo from './AddTodo';
+// import { BrowserRouter, Route, Switch } from 'react-router-dom';
+// import Navbar from './components/Navbar';
+// import Home from './components/Home';
+// import Photos from './components/Photos';
+// import About from './components/About';
+// import Contact from './components/Contact';
+// import FcTable from './components/FcTable';
+// import Table from './components/Table';
+// import Post from './components/Post';;
+import Todos from './Todos';
+import AddTodo from './AddTodo';
 
 class App extends Component {
+  
   state = {
     todos: [
       { id: 1, content: 'todo1' },
       { id: 2, content: 'todo2' }
-    ]
+    ],
+    content: ''
   }
   // deleteTodo = (id) => {
   //   // console.log(id);
@@ -34,27 +36,50 @@ class App extends Component {
   //   let todos = [...this.state.todos, newTodoItem];
   //   this.setState({ todos });
   // }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    let todoId = Math.random();
+    let newTodoItem = {
+      id: todoId,
+      content: this.state.content
+    }
+   
+    let todos = [...this.state.todos, newTodoItem];
+    this.setState({ todos, content: '' });
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      content: e.target.value
+    })
+  }
+
   render() {
     return (
-      <BrowserRouter>
-        <div className="App">
-          <Navbar />
-          <Switch>
-            <Route exact path='/' component={Home} />
-            <Route path='/photos' component={Photos} />
-            <Route path='/about' component={About} />
-            <Route path='/contact' component={Contact} />
-            <Route path='/table' component={Table} />
-            <Route path='/fctable' component={FcTable} />
-            <Route path='/:post_id' component={Post} />
-          </Switch>
-        </div>
-      </BrowserRouter>
-      // <div className="todos-app container">
-      //   <h1 className="center blue-text">Todo's</h1>
-      //   <Todos todos={this.state.todos} deleteTodo={this.deleteTodo} />
-      //   <AddTodo addTodo={this.addTodo} />
-      // </div>
+      // <BrowserRouter>
+      //   <div className="App">
+      //     <Navbar />
+      //     <Switch>
+      //       <Route exact path='/' component={Home} />
+      //       <Route path='/photos' component={Photos} />
+      //       <Route path='/about' component={About} />
+      //       <Route path='/contact' component={Contact} />
+      //       <Route path='/table' component={Table} />
+      //       <Route path='/fctable' component={FcTable} />
+      //       <Route path='/:post_id' component={Post} />
+      //     </Switch>
+      //   </div>
+      // </BrowserRouter>
+      <div className="todos-app container">
+        <h1 className="center blue-text">Todo's</h1>
+        <Todos todos={this.state.todos} deleteTodo={this.deleteTodo} />
+        <AddTodo 
+          handleSubmit={this.handleSubmit} 
+          handleChange={this.handleChange} 
+          allState={this.state} 
+          />
+      </div>
     );
   }
 }
